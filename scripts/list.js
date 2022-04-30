efd3d1ab65213ae5da9e7d00f350e8da767ec000
AnthonyLaneJr <101611103+AnthonyLaneJr@users.noreply.github.com>
2022-04-30 13:32:29 -0700

    Assignment #3
    Assignment # 3 for module 105.

--- scripts/list.js
+++ scripts/list.js
@@ -1,32 +1,30 @@
 function displayUsers(usersArray){
-    tmp=(``)
+    
     //travel the array
     for(let i=0;i<usersArray.length;i++){
     //get the data for each user
-    let user=+usersArray[i]
+    let user = usersArray[i];
     //display users
-    console.log(user)
-    // tmp=+(`
-    //     <tr>
-    //             <td>${data.firstName}</td>
-    //             <td>${data.lastName}</td>
-    //             <td>${data.email}</td>
-    //             <td>${data.password}</td>
-    //             <td>${data.gender}</td>
-    //             <td>${data.age}</td>
-    //             <td>${data.phoneNumber}</td>
-    //             <td>${data.address}</td>
-    //             <td>${data.Color}</td>
-    //             <td>${data.payMethod}</td>
-    //         </tr>
-    // `);
-    }
-    // $(`#tableBody`).val(tmp)
+    tmp = `
+        <tr>
+        <td class="tdfName">${user.firstName}</td>
+        <td class="tdlName">${user.lastName}</td>
+        <td class="tdemail">${user.email}</td>
+        <td class="tdpass">${user.password}</td>
+        <td class="tdgen">${user.gender}</td>
+        <td class="tdage">${user.age}</td>
+        <td class="tdphone">${user.phone}</td>
+        <td class="tdaddress">${user.address}</td>
+        <td class="tdcolor">${user.color}</td>
+        <td class="tdpay">${user.payMethod}</td>
+            </tr>
+    `;
+    $(`#tableBody`).append(`${tmp}`);
     //append the user on the html table ------>read jquery documentation
-}
+}}
 function init(){
     console.log(`Listing Users`);
     let users=readUsers(); //this function is on the store manager-getting the array
     displayUsers(users);
-}
+};
 window.onload=init;
